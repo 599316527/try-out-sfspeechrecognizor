0f5c5993c9aa1b6e9d5f34cc3405791b3385a133
Kyle <hekai1229@gmail.com>
2019-06-11 16:18:28 +0800

    Initial Commit

---
+++ SpeechRecognizorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecognizorTest
+//
+//  Created by Kyle He on 2019/6/11.
+//  Copyright © 2019 hekai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
